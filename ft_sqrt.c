ee5a53174cf52a576ce97f6799c83d46ef07ae39
herrfelix96 <felix_harutyunyan@yahoo.com>
2025-09-01 00:56:52 +0200

    Add files via upload

--- ft_sqrt.c
+++ ft_sqrt.c
@@ -11,39 +11,97 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int		ft_sqrt(int nb);
+#define MAXVALUE 100
+
+void	ft_to_zero(void);
+int	ft_sqrt(int nb);
 void	ft_prime_factors(int nb);
-char	FACTORS[10];
-int main(void)
+int	FACTORS[MAXVALUE];
+int	UNIQUE_FACTORS[MAXVALUE];
+/*
+int main(int argv, char *argc[])
 {
-	int num = 6545;
-	ft_prime_factors(num);
-	//printf("%s\n", FACTORS);
+	if (argv != 2)
+		return (1);
+
+	int num = atoi(argc[1]);
+	printf("%i\n", ft_sqrt(num));
 	return 0;
+}*/
+
+int	ft_sqrt(int nb)
+{
+	int	result;
+	int	i;
+	int	u;
+
+	result = 1;
+	u = 0;
+	i = 0;
+	ft_prime_factors(nb);
+	if (FACTORS[i] != FACTORS[i + 1] || nb <= 0)
+		return (0);
+
+	while (FACTORS[i] == FACTORS[i + 1] && FACTORS[i] != 0)
+	{
+		UNIQUE_FACTORS[u] = FACTORS[i];
+		i = i + 2;
+		u++;
+	}
+	if (FACTORS[i] != 0)
+                return (0);
+	u = 0;
+	while (UNIQUE_FACTORS[u] != 0)
+	{
+		result = result * UNIQUE_FACTORS[u];
+		u++;
+	}
+	return (result);
 }
 
 void	ft_prime_factors(int nb)
 {
-	int		f;
-	int		c;
-	int		nb_copy;
-
+	int	f;
+	int	c;
+	int	nb_copy;
+	
+	ft_to_zero();
 	nb_copy = nb;
 	c = 0;
 	f = 2;
 	while (nb > 0)
 	{
-		if (f  > nb_copy / 2)
+		if (f  > nb_copy)
 			return;
 		if (nb % f == 0)
 		{
-			FACTORS[c] = f + '0';
+			FACTORS[c] = f;
 			c++;
 			nb = nb / f;
-			printf("%i\n", f);
+			//printf("%i\n", f);
 			f = 1;
 		}
 		f++;
 	}
 }
+
+void	ft_to_zero(void)
+{
+	int	i;
+
+	i = 0;
+        while (i < MAXVALUE)
+        {
+                FACTORS[i] = 0;
+                i++;
+        }
+	i = 0;
+	while (i < MAXVALUE)
+	{
+		UNIQUE_FACTORS[i] = 0;
+		i++;
+	}
+}
+
